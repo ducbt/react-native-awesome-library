e2e45008c76e984b3afdc9487bd953ada629e362
Ducbt <ducbtios@gmail.com>
2023-12-22 11:23:51 +0700

    First init
    example project

---
+++ src/View/AwesomeView.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AwesomeView = () => {
+  return <View style={{ flex: 1, backgroundColor: 'red' }} />;
+};
+
+export default AwesomeView;
